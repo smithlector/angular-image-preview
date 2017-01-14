0ee0cef2601275651aaa2026c244913d6750630d
Prembeagle <prem.rai@burpy.com>
2017-01-14 18:05:51 +0530

    preview with attr wrapper

--- script/preview.js
+++ script/preview.js
@@ -3,33 +3,22 @@
 	angular.module('ular.greyGoose.preview', [])
 	.provider('$preview', function () {
 		this.$get = ['$window', '$rootScope', '$timeout', function($window, $rootScope, $timeout){
-			function PreviewFactory(){
-				console.log('asdasd');
+			function PreviewFactory(opt){
+				angular.element(opt.element).bind('change', function(){
+					var FR= new FileReader();
+					FR.onload = function(e) {
+						var newDirective = angular.element('<img src="'+e.target.result+'"/>');
+						angular.element(document.querySelector(opt.attr.previewWrapper)).empty().append(newDirective)
+						$compile(newDirective)(opt.scope);
+					};       
+					FR.readAsDataURL( opt.element[0].files[0] );
+				});
 			}
 			function findElement(query, element) {
 				return angular.element((element || document).querySelector(query));
 			}
 			return PreviewFactory;
 		}];
-	}).directive('preview', ['$sce', '$compile', '$preview', function($sce, $compile, $preview){
-		return {
-			restrict : 'A',
-			scope: true,
-			link: function(scope, element, attr) {
-				angular.element(element).bind('change', function(){
-					console.log(element[0].value);
-					console.log(this.files);
-					var FR= new FileReader();
-					FR.onload = function(e) {
-						var newDirective = angular.element('<img src="'+e.target.result+'"/>');
-						angular.element(document.querySelector('#preview')).empty().append(newDirective)
-						$compile(newDirective)(scope);
-					};       
-					FR.readAsDataURL( this.files[0] );
-				});
-				var sliderres = $preview();
-			}
-		};
-	}]);
+	});
 	angular.module('ular.greyGoose', [ 'ular.greyGoose.preview' ]);
 })(window, document);
--- script/script.js
+++ script/script.js
@@ -3,5 +3,13 @@
 	angular.module('myApp', ['ular.greyGoose'])
 	.controller('PreviewController', ['$scope', function($scope){
 		
+	}]).directive('preview', ['$sce', '$compile', '$preview', function($sce, $compile, $preview){
+		return {
+			restrict : 'A',
+			scope: true,
+			link: function(scope, element, attr) {
+				$preview({scope, element, attr});
+			}
+		};
 	}]);
 })(window, document);
