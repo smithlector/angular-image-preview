db662b4e8f41fae1f547cad00da22e1c23b58a9f
Prembeagle <prem.rai@burpy.com>
2017-01-14 18:51:35 +0530

    preview with multiple files

--- script/preview.js
+++ script/preview.js
@@ -2,23 +2,29 @@
 	'use strict';
 	angular.module('ular.greyGoose.preview', [])
 	.provider('$preview', function () {
-		this.$get = ['$window', '$rootScope', '$timeout', function($window, $rootScope, $timeout){
+		this.$get = function(){
 			function PreviewFactory(opt){
 				angular.element(opt.element).bind('change', function(){
-					var FR= new FileReader();
-					FR.onload = function(e) {
-						var newDirective = angular.element('<img src="'+e.target.result+'"/>');
-						angular.element(document.querySelector(opt.attr.previewWrapper)).empty().append(newDirective)
-						$compile(newDirective)(opt.scope);
-					};       
-					FR.readAsDataURL( opt.element[0].files[0] );
+					angular.element(document.querySelector(opt.attr.previewWrapper)).empty();
+					if(opt.element[0].files.length != 0){
+						for(var i = 0; i < opt.element[0].files.length; i++){
+							var FR= new FileReader();
+							FR.onload = function(e) {
+								var newDirective = angular.element('<img src="'+e.target.result+'"/>');
+								angular.element(document.querySelector(opt.attr.previewWrapper)).append(newDirective);
+							};       
+							FR.readAsDataURL( opt.element[0].files[i] );
+						}
+					}else{
+						angular.element(document.querySelector(opt.attr.previewWrapper)).empty().text('0 File selected');
+					}
 				});
 			}
 			function findElement(query, element) {
 				return angular.element((element || document).querySelector(query));
 			}
 			return PreviewFactory;
-		}];
+		};
 	});
 	angular.module('ular.greyGoose', [ 'ular.greyGoose.preview' ]);
 })(window, document);
